a3cfb2e3a2e07b75148e2d2cdf2105087bbedd53
CC-KEH <arbashhussain08@gmail.com>
2023-09-25 20:51:47 +0530

    Login and register

--- server/index.js
+++ server/index.js
@@ -1,12 +1,38 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const mongoose = require("mongoose");
+const User = require('./models/user.model')
+
 app.use(cors());
 app.use(express.json()); // Converts whatever the app gets into json
 
-app.post("/api/register", (req, res) => {
+mongoose.connect("mongodb://localhost:27017/Abstrato");
+app.post("/api/register", async (req, res) => {
   console.log(req.body);
-  res.json({ status: "ok" });
+  try {
+    await User.create({
+      name: req.body.name,
+      email: req.body.email,
+      passwd: req.body.passwd,
+    });
+    res.json({ status: "ok" });
+  } catch (error) {
+    res.json({ status: "error", error: "Duplicate Email" });
+  }
+});
+
+
+app.post("/api/login", async (req, res) => {
+  const user = await User.findOne({
+    email: req.body.email,
+    passwd: req.body.passwd,
+  });
+  if (user) {
+    return res.json({ status: "ok", user: true });
+  } else {
+    return res.json({ status: "error", user: false });
+  }
 });
 
 app.listen(1337, () => {
